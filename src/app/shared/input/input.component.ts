1887c341ceedddb9129955573743406c47944150
Raoul Nelson <nelson.raoula@gmail.com>
2025-06-06 19:25:18 -0400

    Create reusable input component, add validation error msgs, add debug statements

---
+++ src/app/shared/input/input.component.ts
@@ -0,0 +1,17 @@
+import {Component, Input} from '@angular/core';
+import {FormControl} from "@angular/forms";
+
+@Component({
+  selector: 'app-input',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.css']
+})
+export class InputComponent {
+  //@ts-ignore
+  @Input() control: FormControl;
+  @Input() label: string = '';
+
+
+
+
+}
