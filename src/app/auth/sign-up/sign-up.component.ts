a80de37747da845fbd5e7f6309b5b83e6de5bf0b
Raoul Nelson <nelson.raoula@gmail.com>
2025-06-06 18:59:55 -0400

    Create basic sign-up page using Reactive Forms, Add validators

---
+++ src/app/auth/sign-up/sign-up.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.css']
+})
+export class SignUpComponent {
+
+  authForm = new FormGroup({
+    username: new FormControl('',[
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(20)
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(4),
+      Validators.maxLength(20)
+    ]),
+    passwordConfirmation: new FormControl('', [
+      Validators.required,
+      Validators.minLength(4),
+      Validators.maxLength(20)
+    ])
+  })
+
+  onSubmit() {
+
+  }
+}
