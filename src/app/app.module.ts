9876ea0eb32d324de22738cfcd0c07d96d1c0371
Raoul Nelson <nelson.raoula@gmail.com>
2025-06-06 18:46:36 -0400

    Add modules w/ routing rules, add styling library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthModule } from './auth/auth.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
-  ],
+  ],    
   imports: [
     BrowserModule,
+    AuthModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AuthRoutingModule } from './auth-routing.module';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { AppModule } from '../app.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import {SharedModule} from "../shared/shared.module";
+
+
+@NgModule({
+  declarations: [
+    SignInComponent,
+    SignUpComponent
+  ],
+    imports: [
+        CommonModule,
+        ReactiveFormsModule,
+        AuthRoutingModule,
+        SharedModule
+    ]
+})
+export class AuthModule { }
