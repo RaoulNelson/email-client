5a2239f9d69fabf42b66749e60aa99f395ae92c2
Raoul Nelson <nelson.raoula@gmail.com>
2025-06-08 13:39:59 -0400

    Update form with styling, add more error display messages

--- src/app/shared/input/input.component.ts
+++ src/app/shared/input/input.component.ts
@@ -10,8 +10,12 @@ export class InputComponent {
   //@ts-ignore
   @Input() control: FormControl;
   @Input() label: string = '';
+  @Input() inputType: string =''
 
 
-
-
+  showErrors() {
+    // return this.control.errors && this.control.touched && this.control.dirty
+    const { touched, dirty, errors } = this.control;
+    return touched && dirty && errors;
+  }
 }
